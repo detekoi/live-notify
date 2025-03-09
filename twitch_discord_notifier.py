2594071005cdb7afadd0ea2b14c7e593937e8549
Henry Manes <henrymanes@gmail.com>
2025-03-09 13:43:04 -0700

    Add cache busting to thumbnail URLs to preserve historical thumbnails

--- twitch_discord_notifier.py
+++ twitch_discord_notifier.py
@@ -139,7 +139,8 @@ class TwitchStreamInfo:
         self.title = data['title']
         self.viewer_count = data['viewer_count']
         self.started_at = data['started_at']
-        self.thumbnail_url = data['thumbnail_url'].replace('{width}', '1280').replace('{height}', '720')
+        # Add timestamp to thumbnail URL for cache busting
+        self.thumbnail_url = data['thumbnail_url'].replace('{width}', '1280').replace('{height}', '720') + f"?t={int(time.time())}"
         self.language = data['language']
         self.url = f"https://twitch.tv/{data['user_login']}"
     
@@ -426,7 +427,7 @@ def main():
             "title": "Test Stream Title",
             "viewer_count": 42,
             "started_at": datetime.datetime.now(datetime.timezone.utc).isoformat(),
-            "thumbnail_url": "https://static-cdn.jtvnw.net/previews-ttv/live_user_{user_login}-{width}x{height}.jpg",
+            "thumbnail_url": f"https://static-cdn.jtvnw.net/previews-ttv/live_user_{config['twitch']['channel_name'].lower()}-{{width}}x{{height}}.jpg",
             "language": "en"
         }
         test_stream = TwitchStreamInfo(test_data)
